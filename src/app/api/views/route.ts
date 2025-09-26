76b4ad965451b238d93a767114e530df1dd29cc6
Jafar Rahadian <muhammadjafar954@gmail.com>
2025-09-26 09:07:40 +0700

    edit: debug edit

--- src/app/api/views/route.ts
+++ src/app/api/views/route.ts
@@ -1,12 +1,11 @@
+
 import { kv } from '@vercel/kv';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  console.log("Read KV_REST_API_URL:", process.env.KV_REST_API_URL);
-  console.log("Read KV_REST_API_TOKEN:", process.env.KV_REST_API_TOKEN ? "Token Found" : "Token not Found");
 
   if (!process.env.KV_REST_API_URL || !process.env.KV_REST_API_TOKEN) {
-    console.error("Vercel KV Credential not Found.");
+    console.error("Vercel KV Credential not found.");
     return new NextResponse('KV environment variables not configured.', { status: 500 });
   }
 
@@ -14,7 +13,7 @@ export async function GET() {
     const views = await kv.incr('portfolio-views');
     return NextResponse.json({ views });
   } catch (error) {
-    console.error("Gagal terhubung ke Vercel KV:", error);
+    console.error("Failed connected to Vercel KV:", error);
     return new NextResponse('Internal Server Error: Failed to connect to KV store.', { status: 500 });
   }
 }
